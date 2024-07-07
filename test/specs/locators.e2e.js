425ae92fcd3d4f9b2d5b398dc9a4a1e20269e3f9
Graham Mason <grahamtmason@me.com>
2024-07-06 22:09:19 -0400

    first commit

---
+++ test/specs/locators.e2e.js
@@ -0,0 +1,7 @@
+describe('My locators application', () => {
+    it('should have locators', async () => {
+        //replace the URL with your own URL from the server
+        await browser.url(`https://cnt-0cfae466-ade5-43cf-86b1-fda15a2db681.containerhub.tripleten-services.com/`);
+                // our code will be here
+    })
+})
